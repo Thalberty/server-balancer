f4137c2fd3b4654d9ca50226fcabe29430886f06
thalbert <thalbert.miranda@gmail.com>
2021-10-19 23:36:42 -0300

    calculate users_remaing

--- balancer.py
+++ balancer.py
@@ -12,9 +12,9 @@ class ServerBalancer:
     def server_balancer(self, new_users: List[int]):
         """Balance the number of servers.
 
-        Server is an array of max length iqual to max_user, and each value in that
-        list is the number of ticks remaning so the user is removed. When it gets
-        to zero the user is removed.
+        Server is an array of max length iqual to max_user, and each value in
+        that list is the number of ticks remaning so the user is removed. When
+        it gets to zero the user is removed.
 
         Servers is a list of servers.
         """
@@ -22,32 +22,34 @@ class ServerBalancer:
         for user_qnt in new_users:
             self.servers_in_tick(user_qnt)
 
-        return self.servers_historic
+            num_servers = self.get_count_servers()
+            self.servers_historic.append(num_servers)
 
-    # def user_that_fits_in_a_server(server: list, max_users: int):
-    #     if users < max_users and len(server) < users:
+            self.end_tick()
+
+        return self.servers_historic
 
     def servers_in_tick(self, users_qnt):
         for server in self.servers:
-            # user_for_server, next_users = user_that_fits_in_a_server(server)
-            if users_qnt < self.max_users and len(server) < users_qnt:
-                user_for_server = users_qnt \
-                    if users_qnt <= self.max_users else users_qnt - self.max_users
-                fits_in_server = user_for_server - len(server)
-                # fits all users
-                new_ticks = [self.tasks_time] * fits_in_server
-                server.extend(new_ticks)
+            users_remaing = users_qnt.copy()
+            while users_remaing:
+                user_for_server = self.get_user_for_server(users_remaing)
+                server = self.add_user_in_server(user_for_server, server)
+                users_remaing = users_remaing - user_for_server
 
-                if users_qnt > user_for_server:
-                    pass  # repeat the process
+    def get_user_for_server(self, users_qnt):
+        if users_qnt <= self.max_users:
+            return users_qnt
+        else:
+            return users_qnt - self.max_users
 
-                if users_qnt == 0:
-                    continue  # next_ticket()
+    def add_user_in_server(self, user_for_server, server=[]):
 
-        num_servers = self.get_count_servers()
-        self.servers_historic.append(num_servers)
+        fits_in_server = user_for_server - len(server)
+        new_ticks = [self.tasks_time] * fits_in_server
+        server.extend(new_ticks)
 
-        self.end_tick()
+        return server
 
     def get_count_servers(self, servers):
         pass
